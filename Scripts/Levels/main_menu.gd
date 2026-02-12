9a06c6c209d067be9990a6783994fb760bb9e9cc
255-Ping <bchanceyw@gmail.com>
2026-02-11 22:09:18 -0700

    Docs WEB build and small improvements

--- Scripts/Levels/main_menu.gd
+++ Scripts/Levels/main_menu.gd
@@ -1,5 +1,8 @@
 extends Node2D
 
-
-func _on_texture_button_pressed() -> void:
+func _on_play_button_pressed() -> void:
 	SceneLoader.load_scene("res://Scenes/Levels/level_1.tscn")
+
+
+func _on_quit_button_pressed() -> void:
+	get_tree().quit()
